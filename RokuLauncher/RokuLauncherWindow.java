d9c2b38db377789ac59592c46a8e07ae7d02b14f
Daniel Boydelatour <danielboydelatour@yahoo.com>
2024-07-29 00:01:43 -0400

    close video on roku launch close

--- RokuLauncher/RokuLauncherWindow.java
+++ RokuLauncher/RokuLauncherWindow.java
@@ -5,6 +5,9 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -33,6 +36,14 @@ public class RokuLauncherWindow extends JFrame {
 		setSize(winWide, (RokuProperties.BUTTON_HEIGHT.getPropertiesValueAsInt() * count));
 		LayoutManager gl = new GridLayout(count+1, 1);//plus 1 for "close roku video option"
 		setLayout(gl);
+		this.addWindowListener(new WindowAdapter()
+		{
+		    @Override
+		    public void windowClosing(WindowEvent e)
+		    {
+		      RokuLauncher.closeRokuVideo();
+		    }
+		});
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
