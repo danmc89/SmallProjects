71a8a63d61ed88cccd5c6f776862728e6aaab9df
Daniel Boydelatour <danielboydelatour@yahoo.com>
2024-08-05 03:20:15 -0400

    remove unused imports

--- RokuLauncher/RokuLauncherWindow.java
+++ RokuLauncher/RokuLauncherWindow.java
@@ -12,8 +12,6 @@ import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
